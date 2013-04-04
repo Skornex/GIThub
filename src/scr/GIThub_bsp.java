fa84ecfb0588467b7ff41d61babd80b4bb7d7708
Skornex <daniel.reitz@htl-klu.at>
2013-04-04 11:10:05 +0200

    First Commit

---
+++ src/scr/GIThub_bsp.java
@@ -0,0 +1,18 @@
+package scr;
+
+public class GIThub_bsp {
+
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GIThub");
+		
+
+	}
+
+}
